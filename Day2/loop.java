b188ee2aeaceff95c4ef7827244c0f218e094bf4
Hunter-420 <khanalnischal2075@gmail.com>
2023-04-30 23:10:07 +0545

    for,while,do-while loop

---
+++ Day2/loop.java
@@ -0,0 +1,30 @@
+/*
+    loops in java
+    -   for 
+    -   while 
+
+
+*/
+
+public class loop {
+    public static void main(String[] args) {
+        System.out.println("For loop :");
+        for (int i = 0; i < 11; i++) {
+            System.out.println(i);
+        }
+        System.out.println("");
+        System.out.println("While loop :");
+        int i=1;
+        while (i<5) {
+            System.out.println("github.com/Hunter-420");
+            i++;
+        }
+        System.out.println("");
+        System.out.println("do while loop :");
+        i=1;
+        do {
+            System.out.println("nischal.khanal69");
+            i++;
+        } while (i<5);
+    }
+}
