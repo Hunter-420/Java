ecb20e9fa95e45d401371a48f99a202a63ba3eb2
Hunter-420 <khanalnischal2075@gmail.com>
2023-05-01 12:49:15 +0545

    array short quick intro

---
+++ Day3/Array/arrayIntro.java
@@ -0,0 +1,10 @@
+/*
+ 
+Array --> List of items of the same type
+----------------------------------------
+Defination of Array:
+type[]arrayName = new type[size];
+eg:
+int[] marks = new int[10];
+
+*/
