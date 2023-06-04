0a8df737d2b67c907c10a94ccb0975d9000a0849
Hunter-420 <khanalnischal2075@gmail.com>
2023-06-04 15:30:54 +0545

    Interface

--- Lab/src/Interface/Interface.java
+++ Lab/src/Interface/Interface.java
@@ -1,2 +1,42 @@
-package Interface;public class Interface {
-}
+package Interface;
+
+interface BEIT{
+    public void useLaptop();
+    public void knowGit();
+};
+interface extraBEIT extends BEIT{
+    public void stackoverflow();
+    static void java(){
+        System.out.println("know java");
+    }
+};
+
+class Implement implements extraBEIT{
+
+    @Override
+    public void useLaptop() {
+        System.out.println("Laptop used");
+    }
+
+    @Override
+    public void knowGit() {
+        System.out.println("Know Git");
+    }
+
+    @Override
+    public void stackoverflow() {
+        System.out.println("Account on stack overflow");
+    }
+};
+
+class Use{
+    public static void main(String[] args) {
+        Implement imple = new Implement();
+        imple.knowGit();
+        imple.stackoverflow();
+        imple.useLaptop();
+        extraBEIT.java();
+
+    }
+};
+
