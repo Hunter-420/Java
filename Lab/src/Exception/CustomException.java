be5b4df96174a36eedc19295ffdd3c190ac73ec0
Hunter-420 <khanalnischal2075@gmail.com>
2023-06-06 05:38:23 +0545

    Abstraction

--- Lab/src/Exception/CustomException.java
+++ Lab/src/Exception/CustomException.java
@@ -2,11 +2,22 @@ package Exception;
 import java.lang.*;
 
 class AmountNotEnoughException extends Exception{
-    public String toString()
-    {
-        return "AmountNotEnoughException";
+
+    public AmountNotEnoughException(String message){
+        super(message);
     }
-    public String getMessage(){
-        return "Amount should be greater than 500";
+}
+
+class CustomException{
+    public static void main(String[] args) throws AmountNotEnoughException{
+         int amount = 400;
+         if (amount<500){
+             throw new AmountNotEnoughException("amount must be greater than 500");
+
+         }
+         else {
+             System.out.println(amount);
+         }
+
     }
 }
--- Lab/src/Polymorphism/PolyNormal.java
+++ Lab/src/Polymorphism/PolyNormal.java
@@ -1,5 +1,17 @@
 package Polymorphism;
 
+/*
+   Polymorphism means "many forms", and it occurs when we have many classes
+   that are related to each other by inheritance.
+
+Inheritance inherit
+attributes and methods from another class. Polymorphism uses those methods to perform different tasks.
+This allows us to perform a single action in different ways.
+
+For example, think of a superclass called Animal that has a method called animalSound().
+Subclasses of Animals could be Pigs, Cats, Dogs, Birds - And they also have their own implementation of an animal
+sound (the pig oinks, and the cat meows, etc.):
+ */
 class Animall{
     public void AnimalSound(){
         System.out.println("Animal Sound");
@@ -13,8 +25,8 @@ class Catt extends Animall{
 
 
     //Can we override private method
-    public void AnimalSound(){
-        System.out.println("Meow");
+    public void AnimalSound(String sound){
+        System.out.println(sound);
     }
 };
 
@@ -27,7 +39,7 @@ public class PolyNormal {
     public static void main(String[] args) {
 
         Catt cat= new Catt();
-        cat.AnimalSound();
+        cat.AnimalSound("Meow");
         cat.size();
 
         Elephantt elephant = new Elephantt();
